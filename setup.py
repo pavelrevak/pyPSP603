8ed99838589207f051d9759accc855ce6dff6004
Pavel Revak <pavel.revak@siemens.com>
2017-12-18 14:18:40 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from distutils.core import setup
-setup(name='psp603',
-      version='0.1',
-      author='Tymm Twillman',
-      author_email='tymmothy@gmail.com',
-      description='GW-Instek PSP603 power supply control module',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Programming Language :: Python',
-          'Intended Audience :: Science/Research',
-          'License :: OSI Approved :: BSD License',
-      ],
-      license='BSD-new',
-      requires=[
-          'pySerial',
-      ],
-      provides=[
-          'psp603',
-      ],
-      py_modules=['psp603'],
-      )
+"""A setup tools based setup module.
+"""
 
+import setuptools
+
+setuptools.setup(name='pypsp',
+    version='0.1',
+    author='Tymm Twillman, Pavel Revak',
+    author_email='tymmothy@gmail.com, pavel.revak@gmail.com',
+    description='GW-Instek PSP power supply control module',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+    ],
+    py_modules=[
+        'psp'
+    ],
+)
